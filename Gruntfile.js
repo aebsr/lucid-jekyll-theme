ef72ee2136a654c7c668d105cefe1e1c67a1912e
Antoine Butler <antoinebsr@gmail.com>
2014-01-24 03:10:52 -0500

    added cssmin as  a dev dependency

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,6 +10,13 @@ module.exports = function (grunt) {
             src: 'assets/css/site.css'
           }
         },
+        cssmin: {
+          combine: {
+            files: {
+              'assets/css/site.min.css': ['assets/css/site.css']
+            }
+          }
+        },
         bump: {
             options: {
                 files: ['package.json', 'bower.json', '_config.yml'],
@@ -21,4 +28,7 @@ module.exports = function (grunt) {
 
     grunt.loadNpmTasks('grunt-bump');
     grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+    grunt.registerTask('default', ['autoprefixer', 'cssmin']);
 };
